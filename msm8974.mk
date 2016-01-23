929e198e498d0a15fac1481dcdf4313f440ce923
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2016-01-23 14:57:21 +0100

    use sony aosp adreno
    Change-Id: I3a2cc7a8d57cc0e1c4f3c2635b535f6fb588a643
    
    Conflicts:
    	msm8974.mk

--- msm8974.mk
+++ msm8974.mk
@@ -34,6 +34,21 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
 
+# Camera
+PRODUCT_PACKAGES += \
+    camera.qcom \
+    CameraWorkaround
+
+PRODUCT_PACKAGES += \
+    libcutilz
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera2.portability.force_api=1
+
+# Boringssl-compat
+PRODUCT_PACKAGES += \
+    libboringssl-compat
+
 # Compatibility with older blobs
 PRODUCT_PACKAGES += \
     libstlport
